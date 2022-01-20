17653c9505c897fb8f7c17a06d719dde896905be
matias9486 <matias9486mdp@gmail.com>
2022-01-19 23:36:01 -0300

    Agregar archivos de proyecto.

---
+++ DesafioSemanal/Context/BlogContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using DesafioSemanal.Model;
+
+namespace DesafioSemanal.Context
+{
+    public class BlogContext:DbContext
+    {
+        public BlogContext() 
+        { }
+
+        //https://www.entityframeworktutorial.net/efcore/entity-framework-core-console-application.aspx
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=HATAKE-PC\SQLEXPRESS01;Database=BlogDB;Trusted_Connection=True;");
+        }
+
+        //Entidades
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+    }
+}
